7421a4fe8c5ed3a3dee65d921818f5e96af62004
dantegrizia <dante.grizia@davinci.edu.ar>
2020-06-04 23:06:11 -0300

    primer commit - nada, solo punto javas

---
+++ dv-ds-20201c-g3/src/main/java/ar/edu/davinci/dvds20201cg3/DvDs20201cG3Application.java
@@ -0,0 +1,13 @@
+package ar.edu.davinci.dvds20201cg3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DvDs20201cG3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DvDs20201cG3Application.class, args);
+	}
+
+}
