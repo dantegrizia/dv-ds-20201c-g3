d62e83e47a0e44f9ce4244c6f02e9abfe4b1a3cd
dantegrizia <dante.grizia@davinci.edu.ar>
2020-06-24 23:58:13 -0300

    Dante - Clase 24 6 - Modelos, Repositorios y Servicios

---
+++ dv-ds-20201c-g3/src/main/java/ar/edu/davinci/dvds20201cg3/repositorio/OrdenItemRepositorio.java
@@ -0,0 +1,18 @@
+package ar.edu.davinci.dvds20201cg3.repositorio;
+
+import java.util.List;
+
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import ar.edu.davinci.dvds20201cg3.modelo.OrdenItem;
+
+
+@Repository("ordenItemRepositorio")
+public interface OrdenItemRepositorio extends JpaRepository<OrdenItem, Long>{
+
+	//public List<OrdenItem> searchByName(@Param("nombre") String name) throws DataAccessException;
+
+}
---
+++ dv-ds-20201c-g3/src/main/java/ar/edu/davinci/dvds20201cg3/repositorio/ProductoRepositorio.java
@@ -0,0 +1,20 @@
+package ar.edu.davinci.dvds20201cg3.repositorio;
+
+import java.util.List;
+
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import ar.edu.davinci.dvds20201cg3.modelo.Producto;
+
+
+@Repository("productosRepositorio")
+public interface ProductoRepositorio extends JpaRepository<Producto, Long>{
+
+	public List<Producto> searchByName(@Param("nombre") String name) throws DataAccessException;
+
+}
+
+
